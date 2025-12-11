56d91be9f67a13434fc96999b1dc363ea39cf7e9
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-11 06:18:31 -0300

    Create passo_2.py

---
+++ passo_2.py
@@ -0,0 +1,16 @@
+with open("server.log", "w") as file:
+  file.write("Nov 12 14:33:01 server sshd[1201]: Failed password for root from 177.233.10.45 port 45821 ssh2\n")
+  file.write("Nov 12 14:34:05 server sshd[1202]: Accepted password for user1 from 192.168.0.10 port 52345 ssh2\n")
+  file.write("Nov 12 14:35:11 server sshd[1203]: Failed password for admin from 177.233.10.45 port 45822 ssh2\n")
+  file.write("Nov 12 14:36:00 server sshd[1204]: Connection closed by 192.168.0.11\n")
+  file.write("Nov 12 14:36:50 server sshd[1205]: Failed password for root from 10.0.0.5 port 50231 ssh2\n")
+
+
+def ler_logs(caminho):
+    with open(caminho, "r") as file:
+      linha = file.readlines()
+      contador = []
+      for linha in file:
+         if "Failed password" in linha:
+            contador 
+
