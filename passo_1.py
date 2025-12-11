35b5b27d0d7a525e13c8a7c1b8da200b10e4f6c7
Guilherme <83481058+45Guilherme@users.noreply.github.com>
2025-12-10 21:28:26 -0300

    Update passo_1.py

--- passo_1.py
+++ passo_1.py
@@ -6,5 +6,8 @@ with open("server.log", "w") as file:
   file.write("Nov 12 14:36:50 server sshd[1205]: Failed password for root from 10.0.0.5 port 50231 ssh2\n")
 
 
- 
+  def ler_logs(caminho):
+    with open('caminho', 'r') as file:
+      linhas = file.readlines()
+      
 
